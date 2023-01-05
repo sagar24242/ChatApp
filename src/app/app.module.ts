28b3be3ef4f40d505a90ba1e2321769c9af2c394
sagar tabiyad <84993250+sagar24242@users.noreply.github.com>
2023-01-05 17:38:56 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { MainComponent } from './main/main.component';
+import { UserComponent } from './user/user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    UserComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/chat.ts
@@ -0,0 +1,14 @@
+import { Message } from "./message";
+
+export class Chat {
+
+    
+    chatId: Number;
+    firstUserName: string;
+    secondUserName: string;
+    messageList: Message[];
+
+    constructor() {
+
+    }
+}
